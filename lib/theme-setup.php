369097f844c06fe15741adad8c5a926736d4c9fa
codedevloper13 <codedevloper13@gmail.com>
2022-08-13 18:13:47 +0530

    Coding The Header & The Search Form
    Coding The Header & The Search Form

---
+++ lib/theme-setup.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Theme basic setup
+ *
+ * @package Atheme
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function _themename_theme_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on understrap, use a find and replace
+		 * to change 'understrap' to the name of your theme in all the template files
+		 */
+		load_theme_textdomain( '_themename', get_template_directory() . '/languages' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Adding Thumbnail basic support
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'script',
+				'style',
+			)
+		);
+
+		/*
+		 * Adding support for Widget edit icons in customizer
+		 */
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/*
+		 * Enable support for Post Formats.
+		 * See http://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support(
+			'post-formats',
+			array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+			)
+		);
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'_themename_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+
+		// Set up the WordPress Theme logo feature.
+		add_theme_support( 'custom-logo' );
+
+		// Add support for responsive embedded content.
+		add_theme_support( 'responsive-embeds' );
+
+}
+
+add_action( 'after_setup_theme', '_themename_theme_setup' );
